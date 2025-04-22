cfd40a51b6c2c4f921ae8df9406ef344cf91d3a9
Sayan Das <sayand031999@gmail.com>
2025-04-22 23:06:24 +0530

    Remove unnecessary lines

--- src/main/java/me/sayandas/App.java
+++ src/main/java/me/sayandas/App.java
@@ -9,6 +9,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Hello world!
@@ -21,14 +22,13 @@ public class App
 //                Paths.get("C:\\Users\\sayan\\Videos\\Captures\\F1 23 2025-04-01 22-50-28.mp4"),
 //                VideoResolution.RES_240p_SD.getWidth(), VideoResolution.RES_240p_SD.getHeight());
 
-        Properties dbConnProps = new Properties();
-        dbConnProps.put("user", "admin");
-        dbConnProps.put("password", "a1d2m3i4n5");
-        System.out.println("dbConnProps = " + dbConnProps);
-        System.out.println("JDBC Drivers: " + DriverManager.drivers().toList());
-        Connection dbConn = DriverManager.getConnection(
-                "jdbc:mysql://videoprocessing-db.c4xok8sm2nsh.us-east-1.rds.amazonaws.com:3306", dbConnProps);
-
+//        Properties dbConnProps = new Properties();
+//        dbConnProps.put("user", "admin");
+//        dbConnProps.put("password", "a1d2m3i4n5");
+//        System.out.println("dbConnProps = " + dbConnProps);
+//        System.out.println("JDBC Drivers: " + DriverManager.drivers().collect(Collectors.toList()));
+//        Connection dbConn = DriverManager.getConnection(
+//                "jdbc:mysql://videoprocessing-db.c4xok8sm2nsh.us-east-1.rds.amazonaws.com:3306", dbConnProps);
 
     }
 }
