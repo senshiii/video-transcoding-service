ba3164d2f5e7bc9f028984bd3bb0fa321510c8ef
Sayan Das <sayand031999@gmail.com>
2025-04-26 16:16:27 +0530

    Logging incoming S3 event

--- src/main/java/me/sayandas/function/TaskQueueingHandler.java
+++ src/main/java/me/sayandas/function/TaskQueueingHandler.java
@@ -9,7 +9,9 @@ public class TaskQueueingHandler implements RequestHandler<S3Event, Boolean> {
 
     @Override
     public Boolean handleRequest(S3Event s3Event, Context context) {
-
+        System.out.println("Records: " + s3Event.getRecords());
+        S3EventNotification.S3EventNotificationRecord record = s3Event.getRecords().get(0);
+        System.out.println("record = " + record);
         return null;
     }
 }
