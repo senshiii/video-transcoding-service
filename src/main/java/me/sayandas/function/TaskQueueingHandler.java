6f43ffb2ac8420b9c0d513567d5927fef99f5441
Sayan Das <sayand031999@gmail.com>
2025-04-26 17:19:30 +0530

    Added log statements printing s3 record details

--- src/main/java/me/sayandas/function/TaskQueueingHandler.java
+++ src/main/java/me/sayandas/function/TaskQueueingHandler.java
@@ -11,6 +11,10 @@ public class TaskQueueingHandler implements RequestHandler<S3Event, Boolean> {
     public Boolean handleRequest(S3Event s3Event, Context context) {
         System.out.println("Records: " + s3Event.getRecords());
         S3EventNotification.S3EventNotificationRecord record = s3Event.getRecords().get(0);
+        S3EventNotification.S3BucketEntity bucket = record.getS3().getBucket();
+        System.out.println("bucket.getName() = " + bucket.getName());
+        System.out.println("bucket.getArn() = " + bucket.getArn());
+        System.out.println("bucket.getOwnerIdentity() = " + bucket.getOwnerIdentity());
         S3EventNotification.S3ObjectEntity entity = record.getS3().getObject();
         System.out.println("entity.getUrlDecodedKey() = " + entity.getUrlDecodedKey());
         System.out.println("entity.getSizeAsLong() = " + entity.getSizeAsLong());
