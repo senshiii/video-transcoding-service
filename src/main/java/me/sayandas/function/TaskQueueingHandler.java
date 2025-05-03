47245603804c686833d2df8f2b03dcb7e599b18e
Sayan Das <sayand031999@gmail.com>
2025-05-04 00:55:14 +0530

    Changed logger instance instantiation. Added default console handler

--- src/main/java/me/sayandas/function/TaskQueueingHandler.java
+++ src/main/java/me/sayandas/function/TaskQueueingHandler.java
@@ -15,11 +15,8 @@ import me.sayandas.video.VideoUtils;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class TaskQueueingHandler implements RequestHandler<S3Event, Boolean> {
